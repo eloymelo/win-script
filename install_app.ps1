3fb2e51eb03031a922381627f9722fdc6c3f4a2c
Eloy Melo <eloymelo@pm.me>
2025-03-10 23:00:56 -0300

    first version

---
+++ install_app.ps1
@@ -0,0 +1,73 @@
+$apps = @(
+	"9P1TBXR6QDCX",
+	"9NKSQGP7F2NH",
+	"9NRSP6BRXBZQ",
+	"9NCBCSZSJRSB",
+	"9MSVKQC78PK6",
+	"9WZDNCRFJ3PR",
+	"Microsoft.WSL",
+	"Eassos.DiskGenius",
+	"Git.Git",
+	"Brave.Brave",
+	"Google.Chrome",
+    "PDFGear.PDFGear",
+	"Guru3D.Afterburner",
+	"Guru3D.RTSS",
+    "qBittorrent.qBittorrent",
+    "Anki.Anki",
+    "SoftDeluxe.FreeDownloadManager",
+    "Parsec.Parsec",
+    "EpicGames.EpicGamesLauncher",
+    "TechPowerUp.GPU-Z",
+    "Audacity.Audacity",
+    "Ubisoft.Connect",
+    "Nilesoft.Shell",
+    "HandBrake.HandBrake",
+    "Blizzard.BattleNet",
+    "StartIsBack.StartAllBack",
+    "AgileBits.1Password",
+    "Microsoft.PowerShell",
+    "Microsoft.VisualStudioCode",
+    "Corsair.iCUE.5",
+    "Nickvision.Parabolic",
+    "Obsidian.Obsidian",
+    "VideoLAN.VLC",
+    "7zip.7zip",
+    "Upscayl.Upscayl",
+    "Proton.ProtonVPN",
+	"Proton.ProtonPass",
+	"Proton.ProtonDrive",
+	"Proton.ProtonMail",
+	"Famatech.RadminVPN",
+    "Python.Python.3.9",
+    "OBSProject.OBSStudio",
+    "GIMP.GIMP",
+    "Notepad++.Notepad++",
+    "OpenJS.NodeJS",
+    "LocalSend.LocalSend",
+    "Logitech.GHUB",
+    "Oracle.JavaRuntimeEnvironment",
+    "Discord.Discord",
+    "CPUID.HWMonitor",
+    "CPUID.CPU-Z",
+    "Cloudflare.WARP",
+    "ALCPU.CoreTemp",
+    "Balena.Etcher",
+    "IObit.IObitUnlocker",
+    "AdrienAllard.FileConverter",
+    "Logitech.LogiTune",
+    "MiniTool.PartitionWizard.Free",
+    "ElectronicArts.EADesktop",
+    "Valve.Steam"
+)
+
+foreach ($app in $apps) {
+    Write-Host "Installing $app..." -ForegroundColor Green
+    
+    # Special case for Battle.net (Requires custom location)
+    if ($app -eq "Blizzard.BattleNet") {
+        winget install $app --silent --accept-package-agreements --accept-source-agreements --location "C:\Program Files (x86)\Battle.net"
+    } else {
+        winget install $app --silent --accept-package-agreements --accept-source-agreements
+    }
+}
